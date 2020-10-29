040b645f5c8d0c93cd3d1649f9bcdfe2b5c6e26e
Kinnar Kansara <1075842+kinnarrk@users.noreply.github.com>
2020-10-28 23:06:26 -0400

    Kafka partition support for producer (#13)

--- src/service/ProducerService.js
+++ src/service/ProducerService.js
@@ -5,7 +5,16 @@ exports.publish = (payloads) => {
     try {
         const Producer = kafka.Producer;
         const client = new kafka.KafkaClient({ kafkaHost: config.kafka_host });
-        const producer = new Producer(client);
+        var options = {
+            // Configuration for when to consider a message as acknowledged, default 1
+            requireAcks: 1,
+            // The amount of time in milliseconds to wait for all acks before considered, default 100ms
+            ackTimeoutMs: 100,
+            // Partitioner type (default = 0, random = 1, cyclic = 2, keyed = 3, custom = 4), default 0
+            // random = 1 actually works for sending on different partitions
+            partitionerType: 1
+        }
+        const producer = new Producer(client, options);
 
         producer.on('ready', async function () {
             let push_status = producer.send(payloads, (err, data) => {
@@ -13,6 +22,7 @@ exports.publish = (payloads) => {
                     console.log('[kafka-producer -> ' + config.kafka_topic + ']: broker operation failed');
                 } else {
                     console.log('[kafka-producer -> ' + config.kafka_topic + ']: broker operation success');
+                    console.log('sent data -> {"topic":{"partition":offset}}: ' + JSON.stringify(data));
                 }
             });
         });
